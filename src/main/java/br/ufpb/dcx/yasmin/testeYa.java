1acf073843e9cf9867e59385d644b2050eb7d06c
yasmin.alves <yasmin.alves@dcx.ufpb.br>
2025-11-11 09:24:13 -0300

    Criando nova classe de teste usando IO

---
+++ src/main/java/br/ufpb/dcx/yasmin/testeYa.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.yasmin;
+
+public class testeYa {
+    void main (){
+        IO.println("Oi");
+        String nome = IO.readln("Digite seu nome");
+        IO.println("Oi"+ nome);
+    }
+}
